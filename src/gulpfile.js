e1a787920e2f0a0b19a954dd24ba4ac7f73bc618
Daria <daria7Vo@yandex.ru>
2020-11-09 18:00:00 +0300

    working gulpfile

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,32 +1,32 @@
-const gulp        = require('gulp');
+const gulp = require('gulp');
 const browserSync = require('browser-sync');
-const sass        = require('gulp-sass');
+const sass = require('gulp-sass');
 const cleanCSS = require('gulp-clean-css');
 const autoprefixer = require('gulp-autoprefixer');
-const rename = require("gulp-rename");
+const rename = require('gulp-rename');
 
-gulp.task('server', function() {
+gulp.task('server', function() {  /* запуск лайв-сервера */
     browserSync.init({
         server: {
             baseDir: "./"
         }
     });
 
-    gulp.watch("src/*.html").on('change', browserSync.reload);
+    gulp.watch("./*.html").on('change', browserSync.reload);  /* отслеживание изменений в файле index.html в реальном времени */
 });
 
-gulp.task('styles', function() {
-    return gulp.src("src/sass/**/*.+(scss|sass)")
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(rename({suffix: '.min', prefix: ''}))
-        .pipe(autoprefixer())
+gulp.task('styles', function() { /* преобразование sass кода в css */
+    return gulp.src('./sass/**/*.+(scss|sass)')
+        .pipe(sass.sync({outputStyle: 'compressed'}).on('error', sass.logError)) /* сжатие файла */
+        .pipe(rename({suffix: '.min', prefix: ''})) /* добавление в название файла "min" */
+        .pipe(autoprefixer()) /* добавление автопрефиксов */
         .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(gulp.dest("src/css"))
-        .pipe(browserSync.stream());
+        .pipe(gulp.dest('./css')) /* сохранение файла в папку css */
+        .pipe(browserSync.stream()); /* показать обновления в браузере */
 });
 
 gulp.task('watch', function() {
-    gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles'));
+    gulp.watch('./sass/**/*.+(scss|sass)', gulp.parallel('styles')); /* отслеживание изменений в файлах css и sass */
 });
 
-gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
+gulp.task('default', gulp.parallel('watch', 'server', 'styles')); /* выполнение всех команд при вызове gulp */
